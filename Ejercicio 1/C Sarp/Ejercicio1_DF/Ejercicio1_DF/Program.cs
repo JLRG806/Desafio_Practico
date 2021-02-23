2c7eb82c6122b367e4acc1ee3576192e9f48262f
Jose Luis <47016453+JLRG806@users.noreply.github.com>
2021-02-23 07:44:48 -0600

    Ejercicio 1

---
+++ Ejercicio 1/C Sarp/Ejercicio1_DF/Ejercicio1_DF/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Ejercicio1_DF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //Declaración de variables a usar.           
+            double pArt, cCli, cPag;
+            int l = 1;
+
+            for (int z = 0; z < l; z++)
+            {
+                try
+                {
+                    Console.WriteLine("\nDigite el precio del articulo.");
+                    //Entrada de datos pArt.
+                    pArt = Double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite la cantidad que lleva el cliente.");
+                    //Entrada de datos cCli.
+                    cCli = Double.Parse(Console.ReadLine());
+
+
+                    cPag = pArt * cCli;
+
+
+
+                    Console.WriteLine($" \n\nPor {cCli} articulos, el cliende debe pagar ${cPag}.");
+
+
+
+                }
+                catch (Exception)
+                {
+                    l++;
+                    Console.WriteLine($" \n\nPor favor ingrese un numero");
+
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
