15478060e4b1f583ed163e11eea65291498bc714
Jose Luis <47016453+JLRG806@users.noreply.github.com>
2021-02-23 08:05:56 -0600

    Ejercicio 2

---
+++ Ejercicio 2/C#/Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            //Declaración de variables a usar.
+            int l1, l2, l3, l4,sum,prod;
+
+            
+            int l = 1;
+
+            for (int z = 0; z < l; z++)
+            {
+                try
+                {
+                    Console.WriteLine("\nDigite el primer numero.");
+                    //Entrada de datos l1.
+                    l1 = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite el segundo numero.");
+                    //Entrada de datos l1.
+                    l2 = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite el tercer numero.");
+                    //Entrada de datos l1.
+                    l3 = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite el cuarto numero.");
+                    //Entrada de datos l1.
+                    l4 = Int32.Parse(Console.ReadLine());
+
+                    sum = l1 + l2;
+                    prod = l3 * l4;
+
+                    Console.WriteLine($" \tLa suma del primer y segundo numero es de: {sum}");
+                    Console.WriteLine($" \tEl producto del tercer y cuarto numero es de: {prod}");
+                   
+
+
+                }
+                catch (Exception)
+                {
+                    l++;
+                    Console.WriteLine($" \n\nPor favor ingrese un numero sin decimales");
+
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
